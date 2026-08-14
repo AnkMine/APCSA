d6ba5475b8c92b32024b51a3f04452d15f74522e
AnkMine <shah.ankan@gmail.com>
2026-08-14 14:48:42 -0700

    Aug14Classwork ALL

---
+++ Week1/Aug14Classwork.java
@@ -0,0 +1,23 @@
+class Bunny {
+
+    public static void main( String[] args )
+    {
+        //Below statements print ASCII art
+        System.out.println("\t(\\/)\n\t(..)\n\t\bo(__)");
+    }
+        
+}
+
+
+class Labels {
+
+    public static void main( String[] args)
+    {
+        //Below statements print two columns of person's name and address
+        System.out.println("Ankan Shah \t\t Ankan Shah");
+        System.out.println("614 S El Monte Ave \t 614 S El Monte Ave");
+        System.out.println("Los Altos, CA 94022 \t Los Altos, CA 94022");
+
+    }
+
+}
