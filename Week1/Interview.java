3f1cf20202aab3d198a7fd8150b7e3333583dfe9
AnkMine <shah.ankan@gmail.com>
2026-08-14 13:37:48 -0700

    Aug 14 Warm-up

---
+++ Week1/Interview.java
@@ -0,0 +1,11 @@
+public class Interview {
+
+
+    public static void main(String[] args) {
+        System.out.println("Q: What is your name? \t A: My name is Lukas.");
+        System.out.println("Q: If you could be an animal, what would it be and why? \t A: I would be a whale because it's aura.");
+
+
+    }
+    
+}
