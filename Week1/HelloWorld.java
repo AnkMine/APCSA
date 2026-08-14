be4aa783259827a00a6b83819c4720995f1dca50
AnkMine <shah.ankan@gmail.com>
2026-08-13 17:53:53 -0700

    complete Aug12th Classwork

---
+++ Week1/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+	public static void main( String[] args )
+	{
+		System.out.println("My Name is Ankan.");
+        System.out.println("My favorite color is yellow.");
+        System.out.println("My favorite artist is NF.");
+        System.out.println("One thing I like to do is solve Rubik's cubes.");
+		
+	}
+}	
