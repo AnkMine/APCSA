72d95a278f48933e2027f675deefc5a0a1be3dae
AnkMine <shah.ankan@gmail.com>
2026-08-21 13:43:07 -0700

    commit to restart computer

--- Week2/Triangle.java
+++ Week2/Triangle.java
@@ -1,19 +1,25 @@
+// calculate area of triangle
 public class Triangle {
 
     public static void main (String[] args)
     {
-        
+        // create variables
         int base;
         int height;
         double area;
 
+        // set variables to a value
         base = 7;
         height = 11;
 
+        // print variables and their value
         System.out.println("base = " + base + "\nheight = " + height);
 
+        // calculate area of triangle
         area = (double) (base * height)/2;
-/*
+        
+        //Allows integers to be printed without the ".0" and doubles to be printed with decimal value
+        /*
         if (area == (int) area)
         {
             System.out.println("The area of the triangle is equal to " + (int) area);
@@ -22,7 +28,9 @@ public class Triangle {
         {
             System.out.println("The area of the triangle is equal to " + area);
         }
-*/      
+        */      
+
+        //print area of the triangle
         System.out.println("The area of the triangle is equal to " + area);
     }
 }
