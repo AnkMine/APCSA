16dc90316291f8cdca374f3d5e6cfa9ea3b420eb
AnkMine <shah.ankan@gmail.com>
2026-08-28 13:38:22 -0700

    fix bugs in Aug28 Warmup

--- APCSA_Weeks/Week3/GoodForm.java
+++ APCSA_Weeks/Week3/GoodForm.java
@@ -1,14 +1,20 @@
 // this code will compile. Even though it was in 1 line of code, there were semicolons to seperate each statement.
 
+// import the Scanner class
 import java.util.Scanner;
 
+// this class calculates the area of a circle given the radius inputed by the user
 public class GoodForm { 
     public static void main(String args[]) {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("I will calculate the area of a circle. Please provide a radius: ");
-        double cat = keyboard.nextDouble();
-        double cow = 3.14;
-        double dog = cow * cat * cat;
-        System.out.println("The area of this circle is " + dog); 
+
+        // declare variables for radius, pi, and area
+        double radius = keyboard.nextDouble();
+        double pi = 3.14;
+        double area = pi * radius * radius;
+
+        // print the area of a circle in a string
+        System.out.println("The area of this circle is " + area); 
     }
 }
