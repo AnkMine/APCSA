6f8bf1706abca1a48287c5d5341f358deb0af387
AnkMine <shah.ankan@gmail.com>
2026-09-11 20:41:24 -0700

    finished Lab#1

--- APCSA_Weeks/DeepSpaceCrisisLab.java
+++ APCSA_Weeks/DeepSpaceCrisisLab.java
@@ -1,13 +1,17 @@
+// imports the Scanner class and allows the usage of 2D arrays
 import java.util.Scanner;
 import java.util.Arrays;
 
-
+// main class for math/trivia space game
 public class DeepSpaceCrisisLab { 
+
+    // main method
     public static void main(String [] args) {
 
-        // question arrays
+        // question arrays (with alien arrays with multiple choice)
         // object arrays allow storage or multiple data types
 
+        // Orbital questions (Easy and Hard), contains {Question string, Integer answer, Error message}
         Object[][] orbitalEasy = {
             {"If a satellite completes 1 orbit every 90 minutes, how many full orbits will it complete in a 24-hour day?", 16, "Incorrect. In 24 hours (1,440 minutes), dividing by 90 gives 16 full orbits!"},
             {"A probe travels at a steady speed of 8 km/s. How far does it travel in 10 seconds?", 80, "Incorrect. Speed times time gives 8 km/s * 10 s = 80 km!"},
@@ -24,6 +28,8 @@ public class DeepSpaceCrisisLab {
             {"If Earth's orbital speed is roughly 30 km/s and a comet travels at 42 km/s at the same distance, what is the percentage difference in their speeds relative to Earth's speed?", 40, "Incorrect. The difference is 12 km/s. (12 / 30) * 100 gives a 40% difference!"}
         };
 
+
+        // Multiple choice trivia questions (Easy and Hard), contains {Question string, Correct Answer, Options array, Error message}
         Object[][] alienEasy = {
             {
                 "Which real planet in our solar system is nicknamed the 'Red Planet' and frequently featured in alien stories?",
@@ -84,6 +90,7 @@ public class DeepSpaceCrisisLab {
             }
         };
 
+        // able to set the game sets using the below variables
         Object[][] easySet;
         Object[][] hardSet;
 
@@ -93,25 +100,32 @@ public class DeepSpaceCrisisLab {
 
         System.out.println("\n---------------" + makeBold("Deep Space Crisis Quiz") + "---------------");
 
-        System.out.println("\n\nPlease choose a mission stream (\"orbital\" or \"alien\"): ");
+        // prompts user to pick a stream
+        System.out.println("\n\nPlease choose a mission stream (\"orbital\" or \"trivia\"): ");
         String selection = sc.nextLine().trim();
 
-        int scoreCounter = 0;
 
+
+        // assigns question sets to the game sets depending on user inut
         if (selection.equalsIgnoreCase("orbital")) {
             easySet = orbitalEasy;
             hardSet = orbitalHard;
-        } else if (selection.equalsIgnoreCase("alien")) {
+        } else if (selection.equalsIgnoreCase("trivia")) {
             easySet = alienEasy;
             hardSet = alienHard;
         } else {
             System.out.println("CRITICAL ERROR: Unauthorized stream selection. Aborting launch.");
+            sc.close();
             return;
         }
 
+        // tracks total correct answers throughout the game (score)
+        int scoreCounter = 0;
 
+
+        // asks 3 easy questions using methods: printQuestion and checkAnswer
         for (int i = 0; i < 3; i++) {
-            printQuestion(easySet[i], i + 1);
+            printQuestion(easySet[i], i + 1, false);
 
             if (checkAnswer(sc, easySet[i])) {
                 System.out.println(makeBold("\t\t\tCorrect!"));
@@ -121,12 +135,12 @@ public class DeepSpaceCrisisLab {
             }
         }
 
-
+        // adaptive progression, if score is 3, they the user is able to move on to the harder questions, otherwise stay on easy mode
         if (scoreCounter == 3) {
             System.out.println("\nCongrats! You have moved on to harder questions after completing the above with acceptable accuracy.\n");
 
             for (int i = 0; i < 3; i++) {
-                printQuestion(hardSet[i], i + 1);
+                printQuestion(hardSet[i], i + 1, true);
 
                 if (checkAnswer(sc, hardSet[i])) {
                     System.out.println(makeBold("\t\t\tCorrect!"));
@@ -137,7 +151,7 @@ public class DeepSpaceCrisisLab {
             }
         } else {
             for (int i = 3; i < 6; i++) {
-                printQuestion(easySet[i], i + 1);
+                printQuestion(easySet[i], i + 1, false);
 
                 if (checkAnswer(sc, easySet[i])) {
                     System.out.println(makeBold("\t\t\tCorrect!"));
@@ -148,6 +162,7 @@ public class DeepSpaceCrisisLab {
             }
         }
 
+        // grade table depending on score of user
         String[][] grade = {
             {"F", "Mission Failed — Ship lost in deep space"},
             {"F", "Mission Failed — Ship lost in deep space"},
@@ -160,19 +175,30 @@ public class DeepSpaceCrisisLab {
 
         System.out.println("\n\t\t======================================");
 
+        // prints user grade depending on score of user using the grade 2D array
         System.out.println("\t\t\nFinal Mission Score: " + makeBold(scoreCounter + "") + "/6");
-        System.out.println("\t\t\t\tGrade: " + makeBold(grade[scoreCounter][0] + "") + "(" + grade[scoreCounter][1] + ")\n");
+        System.out.println("\t\t\t\tGrade: " + makeBold(grade[scoreCounter][0] + "") + " (" + grade[scoreCounter][1] + ")\n");
 
         sc.close();
 
 
     }
 
-    // this prints the multiple choice questions by checking length of question array to ensure only the letters are printed for multiple choice questions
-    public static void printQuestion(Object[] questionRow, int questionNum) {
+    // helper method that formats and prints the questions, can handle both multiple choice and free response questions as well as the level of each question
+    public static void printQuestion(Object[] questionRow, int questionNum, boolean questionLevel) {
 
-        System.out.println("\nQuestion " + questionNum + ": " + questionRow[0]);
+        String questionType;
 
+        if (questionLevel == true) {
+            questionType = "Hard";
+        } else {
+            questionType = "Easy";
+        }
+
+        System.out.println("\n" + questionType + " Questions " + questionNum + ": " + questionRow[0]);
+
+        // checks if the game set is multiple choice or free response and if multiple choice, it:
+        // converts the letter entered into a zero-based index to check against the options array
         if (questionRow.length > 3 && questionRow[2] instanceof String[]) {
             String[] options = (String[]) questionRow[2];
             char optionChar = 'A';
@@ -184,18 +210,18 @@ public class DeepSpaceCrisisLab {
         System.out.print("Your answer: ");
     }
 
-    // allows use of nextInt() or nextLine() depending on the type of correctAnswer (int, or str)
+    // helper method that evaluates user input against the correct answer, can handle both integer and string inputs
     public static boolean checkAnswer(Scanner sc, Object[] questionRow) {
 
-        Object[] correctAnswer = questionRow[1];
+        Object correctAnswer = questionRow[1];
 
 
 
-        if (correctAnswer instanceof Integer) { // if correct answer is an integer follow to line 141 if not, go to else if
+        if (correctAnswer instanceof Integer) { // if correct answer is an integer (checks this using instanceof) follow to line 141 if not, go to else if
             try { // allows java to execute code that may give an error
                 int userInt = sc.nextInt();
                 sc.nextLine();
-                return userInt = (Integer) correctAnswer; // return true if the inputed answer by user is correct
+                return userInt == (Integer) correctAnswer; // return true if the inputed answer by user is correct
 
             } catch (Exception e) { // this "catches" a possible error and excecute the following lines
                 sc.nextLine();
@@ -204,11 +230,10 @@ public class DeepSpaceCrisisLab {
         }
 
 
-        if (correctAnswer instanceof String) { // if correct answer is a string, then excecutes following lines, if not goes to the return false; statement
-            
-            String userInput = sc.nextLine().trim();
-            return userInput.equalsIgnoreCase( (String) correctAnswer); // return true if the inputed answer by user is correct
-        }
+
+        // do not need if statement for this as integer inputs already have return statements and the fallback return will call
+        String userInput = sc.nextLine().trim();
+
 
         // Checks for letter choices on multiple-choice questions
         if (questionRow.length > 3 && questionRow[2] instanceof String[]) {
